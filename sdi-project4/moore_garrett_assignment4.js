594ad592890abf69ed67ca82c6814971edf8addf
Garrett Moore <gmoore10@fullsail.edu>
2012-12-12 18:42:44 -0500

    Added Initial Project Files
    Added Initial files for project 4.

---
+++ sdi-project4/moore_garrett_assignment4.js
@@ -0,0 +1,48 @@
+var stringPhoneNumber = function (phoneNumber) {
+};
+
+var stringEmailAddress = function () {
+
+};
+
+var stringURL = function () {
+
+};
+
+var stringCapitalizeAllWords = function () {
+
+};
+
+var stringChangeSeparator = function () {
+
+};
+
+var numberDecimalPlaces = function () {
+
+};
+
+var numberFuzzyMatch = function () {
+
+}
+
+var numberDayDiff = function () {
+
+};
+
+var numberConvertFromString = function () {
+
+};
+
+var arraySmallestValue = function () {
+
+};
+
+var arraySumArray = function () {
+
+};
+
+var arraySortWithKey = function () {
+
+};
+
+//alert("JavaScript works!");
