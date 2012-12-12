b1d70c6bf46235b5079c3d4b49bc785cc0ffa2b5
Garrett Moore <gmoore10@fullsail.edu>
2012-12-12 18:48:01 -0500

    Created Phone Number String Checker Function
    Created a function to check if the string provided is in a valid U.S.
    format (country code excluded).

--- sdi-project4/moore_garrett_assignment4.js
+++ sdi-project4/moore_garrett_assignment4.js
@@ -1,5 +1,26 @@
 var stringPhoneNumber = function (phoneNumber) {
+    var firstDash = phoneNumber.indexOf("-");
+    var secondDash = phoneNumber.lastIndexOf("-");
+
+    if (phoneNumber.search(/\d{3}\-\d{3}\-\d{4}/) == -1) {
+        console.log(phoneNumber + " is not valid phone number.");
+    } else {
+        var prefix = phoneNumber.substring(0, firstDash)
+        var middle = phoneNumber.substring(firstDash + 1, secondDash);
+        var end = phoneNumber.substring(secondDash + 1, phoneNumber.length);
+
+        if (isNaN(prefix) === false && isNaN(middle) === false && isNaN(end) === false) {
+            console.log(phoneNumber + " is a valid phone number.");
+        }
+        else {
+            console.log(phoneNumber + " is not a valid phone number.");
+        }
+    }
 };
+
+stringPhoneNumber("407-679-6333");
+stringPhoneNumber("abc-dec-6333");
+stringPhoneNumber("hello!");
 
 var stringEmailAddress = function () {
 
